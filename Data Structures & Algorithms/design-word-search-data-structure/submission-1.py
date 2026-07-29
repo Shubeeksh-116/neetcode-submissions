c3e32c6f2b7608b984d2812fee81be37f70d386c
Shubeeksh-116 <1ms20cs116@gmail.com>
2026-07-29 14:43:36 +0530

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,38 @@
+class Trienode():
+    def __init__(self):
+        self.children = {}
+        self.word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Trienode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Trienode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        
+        def dfs(j,root):
+            cur = root
+
+            for i in range(j,len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+        return dfs(0, self.root)
+
